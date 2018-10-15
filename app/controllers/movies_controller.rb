c18733297f7dc09f1ed571261484489efdf80094
Santosh <santosh.poudyal@gmail.com>
2018-10-16 09:55:55 +1100

    added edit button after each movie in index page, linked index page with edit page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,11 @@
 class MoviesController < ApplicationController
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
+    
   end
 
   def create
@@ -10,7 +13,8 @@ class MoviesController < ApplicationController
     @movie = Movie.new(movie_params)
     @movie.save
     
-    redirect_to @movie
+    # redirect_to @movie
+    redirect_to movie_path(@movie.id)
   end
 
   private def movie_params
--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
 
   get "movies", to:"movies#index"
   post "movies",to:"movies#create"
-  get "movies/new",to: "movies#new"
-  get "movies/:id/edit",to:"movies#edit"
+  get "movies/new",to: "movies#new", as: "new_movie"
+  get "movies/:id/edit",to:"movies#edit", as: "edit_movie"
   get "movies/:id",to:"movies#show", as: "movie"
   put "movies/:id",to:"movies#update"
   patch "movies/:id",to:"movies#update"
